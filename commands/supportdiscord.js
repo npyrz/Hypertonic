e6f881f0acdd8b9eab1e5f912f7cea6b97952b71
Glitch (hello-express) <none>
2019-10-19 18:35:34 +0000

    🐷👣 Checkpoint ./commands/help.js:6922469/2 ./commands/supportdiscord.js:6922469/1242

--- commands/supportdiscord.js
+++ commands/supportdiscord.js
@@ -4,7 +4,6 @@ const Discord = require("discord.js");
       let botembed = new Discord.RichEmbed()
   .setColor("#FF0000")
   .setDescription("**__Support Discord__** https://discord.gg/7QK4TVb ")
-  .setImage("https://cdn.discordapp.com/attachments/635163040722976800/635180309033385984/210cc55c179bd56ab87c3c6e5e3cb50b2afa3146.webp")
   .setThumbnail("https://cdn.discordapp.com/attachments/633086365093068823/634963215570436096/2033dd42ec0782213771fa7c8c3e50c5.jpg")
   .setFooter("Nava Developers")
   .setTimestamp();
