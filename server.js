18302863cbac5a8c81bf860dffb033d3c05b2051
Glitch (hello-express) <none>
2019-11-18 23:43:29 +0000

    🌦❣️ Checkpoint ./server.js:6922469/3194 ./commands/np.js:6922469/337 ./commands/pause.js:6922469/1095 ./commands/play.js:6922469/2544

--- server.js
+++ server.js
@@ -55,26 +55,45 @@ fs.readdir("./commands/", (err, files) => {
 
 
 
-bot.on('message', message => {
-
-   if (!message.guild) return;
- 
-   if (message.content === '!join') {
-     
-     if (message.member.voiceChannel) {
-       message.member.voiceChannel.join()
-         .then(connection => { 
-           message.reply('I have joined the channel!');
-         })
-         .catch(console.log);
-     } else {
-       message.reply('No can do pal!');
-     }
-   }
- });
 
 
 
+client.once('ready', () => console.log('READY!'));
+client.on('message', message => {
+	if (!message.content.startsWith(client.config.prefix) || message.author.bot) return;
+	const args = message.content.slice(client.config.prefix.length).split(/ +/);
+	const commandName = args.shift().toLowerCase();
+	const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+	if (!command) return;
+	if (command.guildOnly && message.channel.type !== 'text') return message.reply('I can\'t execute that command inside DMs!');
+	if (command.args && !args.length) {
+		let reply = `You didn't provide any arguments, ${message.author}!`;
+		if (command.usage) reply += `\nThe proper usage would be: \`${client.config.prefix}${command.name} ${command.usage}\``;
+		return message.channel.send(reply);
+	}
+	if (!client.cooldowns.has(command.name)) {
+		client.cooldowns.set(command.name, new Collection());
+	}
+	const now = Date.now();
+	const timestamps = client.cooldowns.get(command.name);
+	const cooldownAmount = (command.cooldown || 3) * 1000;
+	if (timestamps.has(message.author.id)) {
+		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+		}
+	}
+	timestamps.set(message.author.id, now);
+	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+	try {
+		command.execute(message, args);
+	} catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
+	}
+});
 
 
 
@@ -84,4 +103,7 @@ bot.on('message', message => {
 
 
 
-bot.login(NjE3Nzg0NzUzNDgyODkxMzE2.XdMRYg.t278541pY10cOjTciFdyBtF1_Zc);
+
+
+
+bot.login('NjE3Nzg0NzUzNDgyODkxMzE2.XdMRYg.t278541pY10cOjTciFdyBtF1_Zc');
