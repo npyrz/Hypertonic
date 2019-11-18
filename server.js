709e482a584de4f5124ed485e4d38501ad405f40
Glitch (hello-express) <none>
2019-11-18 23:31:29 +0000

    🍣🌺 Checkpoint ./server.js:6922469/2

--- server.js
+++ server.js
@@ -84,4 +84,4 @@ bot.on('message', message => {
 
 
 
-bot.login('NjE3Nzg0NzUzNDgyODkxMzE2.XdMRYg.t278541pY10cOjTciFdyBtF1_Zc');
+bot.login(NjE3Nzg0NzUzNDgyODkxMzE2.XdMRYg.t278541pY10cOjTciFdyBtF1_Zc);
