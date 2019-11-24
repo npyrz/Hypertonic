a8c0ce4abe4f6058ca9d4d527d1e9534d37ad384
Glitch (hello-express) <none>
2019-11-24 02:36:36 +0000

    📰🐽 Checkpoint ./commands/play.js:6922469/281

---
+++ commands/play.js
@@ -0,0 +1,13 @@
+const ytdl = require("ytdl-core")
+
+exports.run = async (client, message, args, ops) => {
+  if(!message.member.voiceChannel) return message.channel.send("Please connect to a voice channel!");
+  
+  if(message.guild.me.voiceChannel)
+  
+  
+  
+  
+  
+  
+}
