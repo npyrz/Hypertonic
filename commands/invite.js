f20e09648daf6c4d163802afa7f58a67bf3d0970
Noah <neoptunium@gmail.com>
2021-01-13 21:16:21 -0600

    Add files via upload

--- commands/invite.js
+++ commands/invite.js
@@ -6,6 +6,7 @@ module.exports.run = async (bot, message, args) => {
     .setDescription(' If your interested in inviting Hypertonic to a server use the link below! Also make sure to join the Support Server for any help!')
     .addField("Website Link:", "[Click Here](https://top.gg/bot/617784753482891316)")
     .addField("Support Server Link:", "[Click Here](https://discord.gg/VjCxgr2)")
+    .addField("Discord Bot Invite Link:", "[Click Here](https://discordapp.com/oauth2/authorize?client_id=617784753482891316&permissions=8&scope=bot)")
     .setThumbnail("https://cdn.discordapp.com/attachments/635162251719868468/653052312167907359/h.jpg")
     .setFooter("🔑Join https://discord.gg/8wBgDk3 for Support!🔑")
     .setTimestamp();
