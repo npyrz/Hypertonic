c0f88c4d6b89482df092ad63a86063b7b36b1957
Noah <neoptunium@gmail.com>
2021-01-13 21:15:47 -0600

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,7 @@ module.exports.run = async (bot, message, args) => {
   let bicon = bot.user.displayAvatarURL;
   let botembed = new Discord.MessageEmbed()
     .setColor("#0e2b82")
-    .setDescription("Thank you for using **Hypertonic**! If you have any questions, suggestions, or bug reports join support discord by doing `!support`! If want to know the all the commands for the bot do `!cmds`! If you want to know the permissions of the commands do `!modperms`! If you would like to donate do `!donate`!  **Remember donating is not mandatory but greatly appreciated so quality  of the discord bot can increase!** Once again thanks for using **Hypertonic**!")
+    .setDescription("Thank you for using **Hypertonic**, if you have any questions, suggestions, or bug reports join support discord by doing `!support`! If want to know the all the commands for the bot do `!cmds`! If you want to know the permissions of the commands do `!modperms`! **Remember donating is not mandatory but greatly appreciated so quality of the discord bot can increase!** Once again thanks for using **Hypertonic**!")
     .setImage("https://cdn.discordapp.com/attachments/633086365093068823/634963597759610900/tenor_3.gif")
     .setThumbnail("https://cdn.discordapp.com/attachments/635162251719868468/653052312167907359/h.jpg")
     .setFooter("🔑Join https://discord.gg/8wBgDk3 for Support!🔑")
