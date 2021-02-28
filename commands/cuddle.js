1571f1dbe5cbf500ddd7d81aabd23f11adb61940
HypnoticSiege <65150090+HypnoticSiege@users.noreply.github.com>
2021-02-28 15:40:24 -0500

    Added Cuddle, Kiss and Slap
    New Fun CMDS

---
+++ commands/cuddle.js
@@ -0,0 +1,18 @@
+  
+const Discord = require('discord.js');
+const superagent = require('superagent');
+
+exports.run = async (client, message, args, tools) => {
+    let personToCuddle = args.join(" ");
+    const { body } = await superagent
+    .get("https://nekos.life/api/v2/img/cuddle");
+    const embed = new Discord.MessageEmbed()
+    .setColor("#ff9900")
+    .setTitle(`${message.author.username} cuddles with ${personToCuddle}`)
+    .setImage(body.url) 
+    .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`);
+    message.channel.send({embed})
+};
+  exports.help = {
+    name: 'cuddle',
+  };
---
+++ commands/kiss.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const superagent = require('superagent');
+
+exports.run = async (client, message, args, tools) => {
+    let personToKiss = args.join(" ");
+    const { body } = await superagent
+    .get("https://nekos.life/api/kiss");
+    
+    const embed = new Discord.MessageEmbed()
+    .setColor("#ff9900")
+    .setTitle(`${message.author.username} kissed ${personToKiss}`)
+    .setImage(body.url) 
+    .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`);
+    message.channel.send({embed})
+};
+  
+  exports.help = {
+    name: 'kiss',
+  };
