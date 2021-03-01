84a574cd65af031cf46ad35b4fa699227feaac0c
Noah <neoptunium@gmail.com>
2021-02-28 23:21:30 -0600

    Update nsfw.js

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   let botembed = new Discord.MessageEmbed()
     .setColor("#0e2b82")
     .setTitle("🍆__**NSFW**__🍆")
-    .setImage(`https://tenor.com/view/coconut-coconut-malled-coconut-mall-mario-kart-lovro-the-coolest-gif-20290663`)
+    .setImage("https://tenor.com/view/coconut-coconut-malled-coconut-mall-mario-kart-lovro-the-coolest-gif-20290663")
     .setFooter("🔑Join https://discord.gg/8wBgDk3 for Support!🔑")
     .setTimestamp();
 
