28b993d82848414bda9a7e7221ee37f40e9aface
HypnoticSiege <65150090+HypnoticSiege@users.noreply.github.com>
2021-03-02 20:05:49 -0500

    Kiss Command Real vers

--- commands/kiss.js
+++ commands/kiss.js
@@ -3,8 +3,6 @@ const superagent = require('superagent');
 
 exports.run = async (client, message, args, tools) => {
     if (!message.mentions.users.first()) return message.reply("You need to mention someone to kiss them!");
-    if (message.mentions.users.first().id == "640305009325506581" && message.author.id != "148764657107075072") return message.channel.send(`DENIED | <@148764657107075072>, ${message.author} tried to kiss hannah!`)
-    if (message.mentions.users.first().id == "148764657107075072" && message.author.id != "640305009325506581") return message.channel.send(`DENIED | <@640305009325506581>, ${message.author} tried to kiss tom!`)
     const { body } = await superagent
     .get("https://nekos.life/api/kiss");
     const embed = new Discord.MessageEmbed()
