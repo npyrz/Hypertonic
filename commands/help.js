6d875c98146c6e7cfef8b1da30856d96fc48efcd
HypnoticSiege <65150090+HypnoticSiege@users.noreply.github.com>
2021-03-17 11:02:55 -0400

    Updated Help with new look/text

--- commands/help.js
+++ commands/help.js
@@ -1,16 +1,21 @@
 const Discord = require("discord.js");
-module.exports.run = async (bot, message, args) => {
-  let bicon = bot.user.displayAvatarURL;
-  let botembed = new Discord.MessageEmbed()
-    .setColor("#0e2b82")
-    .setDescription("Thank you for using **Hypertonic**, if you have any questions, suggestions, or bug reports join support discord by doing `!support`! If want to know the all the commands for the bot do `!cmds`! If you want to know the permissions of the commands do `!modperms`! **Remember donating is not mandatory but greatly appreciated so quality of the discord bot can increase!** Once again thanks for using **Hypertonic**!")
-    .setImage("https://cdn.discordapp.com/attachments/633086365093068823/634963597759610900/tenor_3.gif")
-    .setThumbnail("https://cdn.discordapp.com/attachments/635162251719868468/653052312167907359/h.jpg")
-    .setFooter("🔑Join https://discord.gg/8wBgDk3 for Support!🔑")
-    .setTimestamp();
+module.exports.run = async(bot, message, args) => {
+    let bicon = bot.user.displayAvatarURL;
+    let botembed = new Discord.MessageEmbed()
+        .setColor("#0e2b82")
+        .setTitle("Hypertonic Discord Bot")
+        .setDescription("Thanks for using Hypertonic, a Multi-Use Discord Bot. Our Prefix is ``!`` and you can get a list of all our commands by using ``!cmds``. To view permissions on commands you can use ``!modperms``. For support regarding Hypertonic, you can join our Support Server by using ``!support``.")
+        //.addField('Prefix:', "!")
+        //.addField('Commands:', "!cmds")
+        //.addField('Commands Permissions:', '!modperms')
+        //.addField('Support Server', '!support')
+        .setImage("https://cdn.discordapp.com/attachments/633086365093068823/634963597759610900/tenor_3.gif")
+        .setThumbnail("https://cdn.discordapp.com/attachments/635162251719868468/653052312167907359/h.jpg")
+        .setFooter("🔑Join https://discord.gg/8wBgDk3 for Support!🔑")
+        .setTimestamp();
 
-  message.channel.send(botembed);
+    message.channel.send(botembed);
 }
 module.exports.help = {
-  name: "help"
+    name: "help"
 }
