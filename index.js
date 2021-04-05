ec52805efbb9635ffc9084647bfcddd5ebea804f
Noah <neoptunium@gmail.com>
2021-04-05 09:50:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const Discord = require("discord.js");
 const botconfig = require("./botconfig.json");
 const fs = require("fs");
 const prefix = botconfig.prefix;
-const {registerEvents } = require('./handlers/events');
+const { registerEvents } = require('./handlers/events');
 const bot = new Discord.Client({
-  disableEveryone: true,
-  fetchAllMembers: true
+    disableEveryone: true,
+    fetchAllMembers: true
 });
 registerEvents(bot, '../events');
 bot.commands = new Discord.Collection();
@@ -13,36 +13,36 @@ bot.prefix = prefix;
 bot.config = botconfig;
 const CurrentTimers = new Map();
 const client = new Discord.Client();
-let statuses = ['🗯️!help🗯️', '🔑!cmds🔑', '🖥️discord.gg/8wBgDk3🖥️', 'Prefix: !', 'Version 1.3.0'];
-setInterval(function () {
+let statuses = ['🗯️!help🗯️', '🔑!cmds🔑', '🖥️discord.gg/8wBgDk3🖥️', 'Prefix: !', 'Partners: discord.gg/dQWyBmeRgr', 'Version 1.3.0'];
+setInterval(function() {
 
-  let status = statuses[Math.floor(Math.random() * statuses.length)];
+    let status = statuses[Math.floor(Math.random() * statuses.length)];
 
-  bot.user.setPresence({
+    bot.user.setPresence({
 
-    activity: {
-      name: status
-    },
+        activity: {
+            name: status
+        },
 
-    status: 'online'
-  });
+        status: 'online'
+    });
 }, 5000)
 
 
 fs.readdir("./commands/", (err, files) => {
 
-  if (err) console.log(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if (jsfile.length <= 0) {
-    console.log("Couldn't find commands.");
-    return;
-  }
+    if (err) console.log(err);
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if (jsfile.length <= 0) {
+        console.log("Couldn't find commands.");
+        return;
+    }
 
-  jsfile.forEach((f, i) => {
-    let props = require(`./commands/${f}`);
-    console.log(`${f} loaded!`);
-    bot.commands.set(props.help.name, props);
-  });
+    jsfile.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`${f} loaded!`);
+        bot.commands.set(props.help.name, props);
+    });
 });
 
 
