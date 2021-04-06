5c0c9201cfe53b16e2279e1e3f062d72930aaab1
Noah <neoptunium@gmail.com>
2021-04-06 15:44:30 -0500

    Add files via upload

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,21 +1,21 @@
 const Discord = require("discord.js");
 
-module.exports.run = async (bot, message, args) => {
+module.exports.run = async(bot, message, args) => {
 
-  let serverembed = new Discord.MessageEmbed()
-    .setColor("#0e2b82")
-    .setTitle(`${message.guild.name}\'s Information`)
-    .addField("Guild ID:", message.guild.id, false)
-    .addField("Server Owner:", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, false)
-    .addField("Created:", `${require('moment')(message.guild.createdAt).format('ddd, MMMM Do YYYY [at] hh:mm A')} | ${require('moment')(message.guild.createdAt).fromNow()}`, false)
-    .addField("Server Region:", message.guild.region, false)
-    .addField("Members:", message.guild.memberCount, false)
-    .addField("Channels:", message.guild.channels.cache.size, false)
-    .addField("Roles:", message.guild.roles.cache.size, false)
-    .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`)
+    let serverembed = new Discord.MessageEmbed()
+        .setColor("#0e2b82")
+        .setTitle(`${message.guild.name}\'s Information`)
+        .addField("Guild ID:", message.guild.id, false)
+        .addField("Server Owner:", `${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`, false)
+        .addField("Created:", `${require('moment')(message.guild.createdAt).format('ddd, MMMM Do YYYY [at] hh:mm A')} | ${require('moment')(message.guild.createdAt).fromNow()}`, false)
+        .addField("Server Region:", message.guild.region, false)
+        .addField("Members:", message.guild.memberCount, false)
+        .addField("Channels:", message.guild.channels.cache.size, false)
+        .addField("Roles:", message.guild.roles.cache.size, false)
+        .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`)
 
-  message.channel.send(serverembed);
+    message.channel.send(serverembed);
 }
 module.exports.help = {
-  name: "serverinfo"
+    name: "serverinfo"
 }
--- commands/setprefix.js
+++ commands/setprefix.js
@@ -4,29 +4,70 @@ const default_prefix = config.default_prefix
 const db = require("quick.db")
 exports.run = async(client, message, args, tools) => {
     if (!message.member.hasPermission("ADMINISTRATOR")) {
-        return message.channel.send("You cannot change the Prefix in this server!")
+        return message.channel.send(new Discord.MessageEmbed()
+                .setDescription("Sorry, you cannot change the prefix in this server!")
+                .setColor("#0e2b82")
+                .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`))
+            .then(m => m.delete({ timeout: 30000 }))
     }
 
     if (!args[0]) {
-        return message.channel.send("Please provide a Prefix to set!")
+        return message.channel.send(new Discord.MessageEmbed()
+                .setDescription("Please provide a prefix to set!")
+                .setColor("#0e2b82")
+                .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`))
+            .then(m => m.delete({ timeout: 30000 }))
     }
 
+
     if (args[1]) {
-        return message.channel.send("You cannot set a Prefix to a Double Argument!")
+        return message.channel.send(new Discord.MessageEmbed()
+                .setDescription("You cannot set a prefix to a double argument!")
+                .setColor("#0e2b82")
+                .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`))
+            .then(m => m.delete({ timeout: 30000 }))
     }
 
     if (args[0].length > 3) {
-        return message.channel.send("You cannot set a Prefix to more than 3 Characters!")
+        return message.channel.send(new Discord.MessageEmbed()
+                .setDescription("Sorry, you cannot set a prefix to more than 3 characters!")
+                .setColor("#0e2b82")
+                .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`))
+            .then(m => m.delete({ timeout: 30000 }))
     }
 
     if (args.join("") === default_prefix) {
         db.delete(`prefix_${message.guild.id}`)
-        return await message.channel.send("Reset Prefix ✅")
+        return message.channel.send(new Discord.MessageEmbed()
+            .setDescription("Reset Prefix ✅")
+            .setColor("#0e2b82")
+            .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`))
     }
 
     db.set(`prefix_${message.guild.id}`, args[0])
-    await message.channel.send(`Set Prefix to **${args[0]}** for **${message.guild.name}**`)
+    await message.channel.send(new Discord.MessageEmbed()
+        .setDescription(`Set prefix to **${args[0]}** for ${message.guild.name}`)
+        .setColor("#0e2b82")
+        .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`))
 };
+
+
+let embed = new Discord.MessageEmbed()
+    .setTitle(`Set Prefix`)
+    .setColor("#0e2b82")
+    .addField('User Setting the Prefix:', member.user)
+    .addField(`Prefix:`, `${args[0]}`)
+    .setFooter("🔑Join https://discord.gg/8wBgDk3 for Support!🔑")
+    .setTimestamp();
+
+let sChannel = message.guild.channels.cache.find(channel => channel.name === 'bot-logs');
+if (!sChannel) return message.channel.send(new Discord.MessageEmbed()
+        .setDescription("Please create a `bot-logs` channel first!")
+        .setColor("#0e2b82")
+        .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`))
+    .then(m => m.delete({ timeout: 30000 }));
+sChannel.send(embed)
+
 module.exports.help = {
     name: 'setprefix'
 }
