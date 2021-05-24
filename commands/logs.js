e6655baf82c6350cd4fa8dbe6e2de38df0010295
Noah <neoptunium@gmail.com>
2021-05-24 10:23:40 -0500

    update 1.3.3 push #1

--- commands/logs.js
+++ commands/logs.js
@@ -3,11 +3,17 @@ const db = require("quick.db")
 module.exports.run = async(bot, message, args) => {
     let LoggingChannel = db.get(`loggingchannel_${message.guild.id}`)
     if (!LoggingChannel) {
-        message.channel.send(`:x: Could not find a Logging Channel. Please set one with the \`\`setlogs\`\` Command!`)
+            message.channel.send(new Discord.MessageEmbed()
+            .setDescription("Sorry, couldn't find a set logging channel for moderation commands, please set one up with the \`\`setlogs\`\` command!")
+            .setColor("#0e2b82")
+            .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`))
     } else {
-        message.channel.send(`The current Logging Channel is set to <#${LoggingChannel}>`)
+        message.channel.send(new Discord.MessageEmbed()
+            .setDescription(`The current moderation logging channel is set to <#${LoggingChannel}>!`)
+            .setColor("#0e2b82")
+            .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`))
     }
 }
 module.exports.help = {
-    name: "loggingchannel"
+    name: "logs"
 }
--- commands/setlogs.js
+++ commands/setlogs.js
@@ -2,20 +2,31 @@ const Discord = require('discord.js')
 const db = require("quick.db")
 module.exports.run = async(client, message, args) => {
     let permission = message.member.hasPermission("MANAGE_CHANNELS");
-    if (!permission) return message.channel.send(":x: You are missing the permission `MANAGE_CHANNELS`")
+    if (!permission) return message.channel.send(new Discord.MessageEmbed()
+    .setDescription(`Sorry, you don't have permission to set the moderation logging channel!`)
+    .setColor("#0e2b82")
+    .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`))
+
     let LoggingChannel = args[0]
-    if (isNaN(LoggingChannel)) return message.channel.send("You must specify a valid Channel ID for the Logging Channel!")
+    if (isNaN(LoggingChannel)) return message.channel.send(new Discord.MessageEmbed()
+    .setDescription(`You must specify a valid channel ID for the logging channel!`)
+    .setColor("#0e2b82")
+    .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`))
     try {
-        client.channels.cache.get(LoggingChannel).send(":white_check_mark: Server Logging Channel bound here!")
-        db.set(`loggingchannel_${message.guild.id}`, LoggingChannel)
-        message.channel.send(":white_check_mark: You have successfully set the Logging Channel to <#" + LoggingChannel + ">")
+        client.channels.cache.get(LoggingChannel).send(new Discord.MessageEmbed()
+        .setDescription(`Server logging channel set here!`)
+        .setColor("#0e2b82")
+        .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`))
+         db.set(`loggingchannel_${message.guild.id}`, LoggingChannel)
+        message.channel.send(new Discord.MessageEmbed()
+        .setDescription("You have successfully set the logging channel to <#" + LoggingChannel + ">")
+        .setColor("#0e2b82")
+        .setFooter(`🔑Join https://discord.gg/8wBgDk3 for Support!🔑`))
     } catch (e) {
         console.log(e)
         return message.channel.send(":x: Error!")
     }
-
-
 }
 module.exports.help = {
-    name: "setchannel"
+    name: "setlogs"
 }
