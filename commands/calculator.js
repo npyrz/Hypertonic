b095055d97f9bc6996d2d31e72747ba1c0d82e45
Noah <neoptunium@gmail.com>
2021-05-31 15:01:36 -0500

    added missing features of 1.3.3 update

--- commands/calculator.js
+++ commands/calculator.js
@@ -7,7 +7,7 @@ module.exports.run = async(client, message, args) => {
             .setTitle(`Calculator`)
             .addField(`Equation:`, args.join(' '))
             .addField(`Answer:`, evaluate(args.join(' ')))
-            .setColor('BLUE')
+            .setColor('#0e2b82')
         message.channel.send(embed)
     } catch (e) {
         message.reply(`Please supply a valid equation!`)
--- commands/funcmds.js
+++ commands/funcmds.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
   let botembed = new Discord.MessageEmbed()
     .setTitle("**__😎Fun Commands😎__**")
     .setColor("#0e2b82")
-    .setDescription("`dice` | `8ball` | `coinflip` | `nsfw` | `cuddle` | `kiss` | `slap` | `meme` | `hug` | `joke` | `kill` | `lie` | `rps`")
+    .setDescription("`dice` | `8ball` | `coinflip` | `nsfw` | `cuddle` | `kiss` | `slap` | `meme` | `hug` | `joke` | `kill` | `lie` | `rps` | `calculator`")
     .setFooter(message.createdAt)
     .setThumbnail("https://cdn.discordapp.com/attachments/635162251719868468/653052312167907359/h.jpg")
     .setFooter("🔑Join https://discord.gg/8wBgDk3 for Support!🔑")
